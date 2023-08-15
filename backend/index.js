fd81ca7a6ab13ff5d87ede14c74982785fc7d2d1
Dariel Avila <darielavila43@gmail.com>
2023-08-15 11:43:27 +0800

    updating the backend with hashing

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -11,19 +12,32 @@ const con = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "",
-    database: "q1"
+    database: "register"
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     // const firstname = req.body.firstname;
     // const lastname = req.body.lastname;
     // const contact = req.body.contact;
-    // const status = req.body.status;
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
+    const hash = await bcrypt.hash(password, 13);
+    // user.push({
+    //     password: hash
+    // })
 
-    con.query("INSERT INTO users (email, username, password) VALUES (?, ?, ?)", [email, username, password],
+    // con.query("INSERT INTO users (firstname, lastname, contact, status, email, password) VALUES (?, ?, ?, ?, ?, ?)", [firstname, lastname, contact, email, password],
+    //     (err, result) => {
+    //         if (result){
+    //             res.send(result);
+    //         }else{
+    //             req.send({ message: "ENTER CORRECT ASKED DETAILS!" })
+    //         }
+    //     }
+    // )
+
+    con.query("INSERT INTO users (email, username, password) VALUES (?, ?, ?)", [email, username, hash],
         (err, result) => {
             if (result) {
                 res.send(result);
@@ -34,15 +48,31 @@ app.post('/register', (req, res) => {
     )
 })
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+    // const email = req.body.email;
     const username = req.body.username;
-    const password = req.body.password;
-    con.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password],
+    const password = req.body.password.toString();
+    // const user = users.find(u => u.username === username);
+    // if(!user){
+    //     res.send("wrong username");
+    //     return;
+    // }
+    // const isValid = await bcrypt.compare(password, username.password);
+    // if(!isValid){
+    //     res.send("wrong password");
+    //     return;
+    // }
+    con.query("SELECT * FROM users WHERE username = ?", [username, password],
         (err, result) => {
             if (err) {
                 req.setEncoding({ err: err });
             } else {
                 if (result.length > 0) {
+                    bcrypt.compare(password, result[0].password, (err, response)=>{
+                        if(err){
+                            return res.json("Error")
+                        }
+                    })
                     res.send(result);
                 } else {
                     res.send({ message: "WRONG USERNAME OR PASSWORD!" })
@@ -55,3 +85,4 @@ app.post("/login", (req, res) => {
 app.listen(3001, () => {
     console.log("running backend server");
 })
+
